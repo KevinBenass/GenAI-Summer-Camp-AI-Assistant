a8b3124048afa8ee67cd39d5b6e8b1f53700c7d5
KevinBenass <47986404+KevinBenass@users.noreply.github.com>
2023-11-30 14:04:10 +0200

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -15,13 +15,21 @@ llm = llm=OpenAI(openai_api_key=os.environ['OPENAI_API_KEY'],temperature=0)
 # # Initialize instructor embeddings using the Hugging Face model
 instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
 vectordb_file_path = "faiss_index"
+context_file_path = "context.txt"
 
+def get_context(context_file_path):
+     with open(context_file_path, 'r') as file:
+    # Read the content of the file into a string
+        context = file.read()
+     return context
+    
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     docs = [Document(page_content=x) for x in text_splitter.split_text(text)]
     return docs
   
-def create_vector_db(str_context):
+def create_vector_db():
+    str_context= get_context(context_file_path)
     docs = get_text_chunks(str_context)
   
     # Create a FAISS instance for vector database from docs
